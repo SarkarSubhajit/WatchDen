59fca454a7cc38757f819df5e544297fbdaf6326
sarvan0510 <sarvanmanikanta0510@gmail.com>
2026-01-29 11:54:30 +0530

    fixed error

--- backend/user-service/src/main/java/com/watchden/user/entity/UserProfile.java
+++ backend/user-service/src/main/java/com/watchden/user/entity/UserProfile.java
@@ -11,9 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user_profiles")
